2d660ace35c559c83b1fd259d76bbd2f9f2a5f72
neville <124623051+Neville03@users.noreply.github.com>
2023-02-06 20:14:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function onSubmit() {
+
+    
+            if (document.getElementById('password').value == 'n3v1LL3') {window.button2.style.display="block"; }
+            else{ 
+                alert('Access Denied, Please try again');
+            }
+     }
+     
+
+     
